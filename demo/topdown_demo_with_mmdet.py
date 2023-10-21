03b25b8e5e8434f2225740a9b74b5be047a6f0f8
pinto0309 <rmsdh122@yahoo.co.jp>
2023-10-21 15:36:52 +0900

    debug

--- demo/topdown_demo_with_mmdet.py
+++ demo/topdown_demo_with_mmdet.py
@@ -410,7 +410,7 @@ def main():
 
                 # inference
                 pred_instances, pose_results = process_one_image(args, frame, detector, pose_estimator, visualizer)
-                print(pose_results[0].pred_instances)
+                print(pose_results[0].pred_instances._data_fields)
 
                 if args.save_predictions:
                     pred_instances_list.append(dict(frame_id=index+1, instances=split_instances(pred_instances)))
